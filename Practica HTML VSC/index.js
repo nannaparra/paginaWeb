15ed46b56a89d07bb5a4787ddfb7213bb0191dce
nannaparra <43254886+nannaparra@users.noreply.github.com>
2020-05-25 22:05:24 -0300

    Ultima actualización

---
+++ Practica HTML VSC/index.js
@@ -0,0 +1,13 @@
+
+
+function msjEnviado(){
+    alert("Mensaje Enviado");
+}
+
+function setImgProd(){
+    document.getElementById("imgGrande").src="img/foto2.jpg"
+}
+
+function selectTalle(){
+    document.getElementById("btnTalle").style.backgroundColor='255,255,255';
+}
